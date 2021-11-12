eddeae339c130887e1b33e2ffc7099cb46922f2e
Jimmy Sweeney <70237579+sweenejp@users.noreply.github.com>
2021-11-11 23:30:03 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+const loadingUsersDOM = document.querySelector('.loading-users');
+const loadingPostsDOM = document.querySelector('.loading-posts');
+const usersDOM = document.querySelector('.users');
+const postsDOM = document.querySelector('.posts');
+
+const getAndShowUsers = async () => {
+  loadingUsersDOM.style.visibility = 'visible';
+  try {
+    const { data } = await axios.get(`${baseUrl}/users`);
+    const usersElements = data
+      .map((user) => {
+        const { name, username, id } = user;
+        return `<tr onClick="getAndShowPosts(${id})"><td>${name}</td></tr>`;
+      })
+      .join('');
+    usersDOM.innerHTML = `<th>Authors</th>${usersElements}`;
+  } catch (error) {
+    usersDOM.innerHTML =
+      '<h5 class="error">There was an error, please try later....</h5>';
+  }
+  loadingUsersDOM.style.visibility = 'hidden';
+};
+
+getAndShowUsers();
+
+const getAndShowPosts = async (id) => {
+  loadingPostsDOM.style.visibility = 'visible';
+  console.log(id);
+  try {
+    const { data } = await axios(`${baseUrl}/users/${id}/posts`);
+    const postsElements = data
+      .map((post) => {
+        const { title, body } = post;
+        return `<article class="post"><h2 class="post-title">${title}</h2><p class="post-body">${body}</p></article>`;
+      })
+      .join('');
+    postsDOM.innerHTML = postsElements;
+  } catch (error) {
+    postsDOM.innerHTML =
+      '<h5 class="error">There was an error, please try later....</h5>';
+  }
+  loadingPostsDOM.style.visibility = 'hidden';
+};
